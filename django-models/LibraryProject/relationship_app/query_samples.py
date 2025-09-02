0ff0dc7b6dd49d174ab04cb0a8bd20dc980c2b44
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-02 17:08:13 +0300

    Add a query for models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,19 +5,10 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 
-def query_books_by_author(author_name):
+def list_all_books_in_library():
     """
-    Function to query books by a specific author
+    Function that lists all the books in a library
     """
-    try:
-        author = Author.objects.get(name=author_name)
-        books = Book.objects.filter(author=author)
-        return books
-    except Author.DoesNotExist:
-        return []
-
-
-def list_libraries_with_books():
     """
     Function to list all libraries with their books
     """
@@ -26,17 +17,3 @@ def list_libraries_with_books():
     for library in libraries:
         library_books[library.name] = library.books.all()
     return library_books
-
-
-def retrieve_librarian_for_library(library_name):
-    """Retrieve the librarian for a library"""
-    try:
-        librarian = Librarian.objects.get(name=library_name)
-        librarian = Librarian.librarian
-        return librarian
-    except Library.DoesNotExist:
-        print(f"Library {library_name} does not exist")
-        return None
-    except Librarian.DoesNotExist:
-        print(f"No librarian assigned to library {library_name}")
-        return None
