934ac8091fab44de8ff64e9d6e7ae7ed34c8b83a
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-02 16:04:56 +0300

    Add a relationship app to library

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField()
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField()
