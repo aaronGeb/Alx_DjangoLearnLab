49332445e857443c26da1038118d6eea73f94266
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-02 17:20:24 +0300

    Add a query for models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,7 +9,8 @@ def get_books_by_author(author_name):
     """
     Return all books written by a given author.
     """
-    return Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 
 def get_books_in_library(library_name):
