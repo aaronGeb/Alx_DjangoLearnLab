1ab96156a4a0229fed1802339125abd4000a2f48
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-02 17:00:01 +0300

    Add a query for models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -31,34 +31,6 @@ def list_libraries_with_books():
 def retrieve_librarian_for_library(library_name):
     """Retrieve the librarian for a library"""
     try:
-        library = Library.objects.get(name=library_name)
-        librarian = library.librarian
-        return librarian
-    except Library.DoesNotExist:
-        print(f"Library {library_name} does not exist")
-        return None
-    except Librarian.DoesNotExist:
-        print(f"No librarian assigned to library {library_name}")
-        return None
-
-
-def retrieve_librarian_for_library(library_name):
-    """Retrieve the librarian for a library"""
-    try:
-        library = Library.objects.get(name=library_name)
-        librarian = library.librarian
-        return librarian
-    except Library.DoesNotExist:
-        print(f"Library {library_name} does not exist")
-        return None
-    except Librarian.DoesNotExist:
-        print(f"No librarian assigned to library {library_name}")
-        return None
-
-
-def retrieve_librarian_for_library(library_name):
-    """Retrieve the librarian for a library"""
-    try:
         librarian = Librarian.objects.get(name=library_name)
         librarian = Librarian.librarian
         return librarian
