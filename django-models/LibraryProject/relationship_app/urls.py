f392f29397beab91be0a14dec918a3acec586532
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-05 13:32:07 +0300

    Add templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from . import views
-#from .views import ListBooksView,list_all_books_db
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .views import list_books, LibraryDetailView
+from .views import LoginView, RegisterView, LogoutView
+from django.views.generic import TemplateView
 urlpatterns = [
     path("all_books/", list_books, name="all_books"),
     path("class_listbooks/", LibraryDetailView.as_view(),
          name="all_books_in_library"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("", TemplateView.as_view(template_name="relationship_app/home.html"), name="home"),
+  
 ]
 
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-
+from django.views import View
+from django.contrib.auth.forms import UserCreationForm
+from dajngo.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib import messages
 
 # Create your views here.
 def list_books(request):
@@ -23,3 +28,44 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["books"] = self.objects.all()
         return context
+
+class LoginView(View):
+    template_name = 'relationship_app/login.html'
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        # Handle login logic here
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f"Welcome back, {user.username}!")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+            return render(request, self.template_name, {"form": form})
+        
+class RegisterView(View):
+    template_name = 'relationship_app/register.html'
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, f"Account created for {user.username}!")
+            return redirect("home")
+        else:
+            messages.error(request, "Please correct the error below.")
+            return render(request, self.template_name, {"form": form})
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.info(request, "You have successfully logged out.")
+        return redirect("login")
