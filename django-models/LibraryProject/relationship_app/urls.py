3fa02d4860119c54b47734b7bd3e946cadd00979
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-05 11:36:00 +0300

    Add view and url

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("all_books/", views.list_all_books_db, name="all_books"),
-    path("all_books_in_library/", ListBooksView.as_view(),
+    path("class_listbooks/", ListBooksView.as_view(),
          name="all_books_in_library"),
 ]
