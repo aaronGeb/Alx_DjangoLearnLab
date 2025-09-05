4effd99fe0937e55ff070b2412da6dda15d2aaff
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-05 11:59:00 +0300

    Add view and url

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from relationship.models import Book
+from relationship.models import Book, Library
 from django.views.generic import  DetailView
 
 
@@ -8,11 +8,7 @@ from django.views.generic import  DetailView
 def list_all_books_db(request):
     """function-based view to list all books in the database"""
     books = Book.objects.all()
-    output_lines = []
-    for book in books:
-        output_lines.append(f"Title: {book.title}, Author: {book.author.name}")
-    response_text = "\n".join(output_lines)
-    return HttpResponse(response_text, content_type="text/plain")
+    return render(request, "./templates/list_books.html", {"books": books})
 
 
 class ListBooksView(DetailView):
@@ -20,6 +16,7 @@ class ListBooksView(DetailView):
 
     model = Library
     context_object_name = "library"
+    template_name = "./templates/library_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
