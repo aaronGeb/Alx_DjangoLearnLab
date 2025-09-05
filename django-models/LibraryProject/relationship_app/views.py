9b6e29f97aaa05138af6a921f272e17a4a4592ae
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-05 12:02:47 +0300

    Add view and url

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from relationship.models import Book, Library
+from .models import Book, Library
 from django.views.generic import  DetailView
 
 
