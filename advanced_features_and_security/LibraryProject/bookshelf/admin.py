c129c97aa473ae0ac1566fbd3166b9ead54470a1
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-08 15:08:07 +0300

    Add admin page

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 
@@ -9,3 +11,55 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     list_filter = ("publication_year",)
     search_fields = ("title", "author")
+
+
+@admin.register(CustomUser, CustomUserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "email", "first_name", "last_name", "phone_number")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (_("Personal Info"), {"fields": ("first_name", "last_name", "phone_number")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important Dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Fields when creating a new user from the admin
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "phone_number",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+    ordering = ("username",)
