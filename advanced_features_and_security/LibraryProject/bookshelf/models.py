ac242b4d2ae57aedf0e312a1c895db8674caaebd
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-08 14:17:00 +0300

    Add custom users

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+
+
+class CustomUser(models.Model):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField(upload_to='profile_pictures/')
+    
+
+
+    def __str__(self):
+        return self.username
