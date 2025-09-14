0754e2dd9f6d554554dffcade62044939f0230fd
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-14 15:00:33 +0300

    Add a config in the setting

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 from django.contib.auth.models import Permission
 from bookshelf.models import CustomUser
+from .forms import ExampleForm, BookForm
+
 # Create your views here.
