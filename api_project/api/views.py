9aa940f2242574b077a26d49a41b5d72ff64949b
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-20 13:48:28 +0300

    Add view and settings

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .serializers import BookSerializer
 from .models import Book
 
@@ -8,10 +11,14 @@ from .models import Book
 
 
 class BookList(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-q&)_l5b2433@qh15%401y2)68gppv5h#_y!vvnz-bhiuu5m%=l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "rest_framework.authtoken"
 ]
 
 MIDDLEWARE = [
