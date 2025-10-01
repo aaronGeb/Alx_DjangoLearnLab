32c2a5066c22915ea90ff0bef2b8a2658d08b413
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-10-01 16:10:39 +0300

    Add a folder structure

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import login, 
+from django.contrib.auth import login 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.form import AuthenticationForm
 from .forms import CustomUserCreationForm
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -38,3 +44,49 @@ def logout_view(request):
 @login_required
 def profile_view(request):
     return render(request, "profile.html", {"user": request.user})
+
+
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+
+# Detail of a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+
+# Create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Update an existing post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Delete a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'posts/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -16,7 +16,7 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.ENV.read_env(os.path.join(BASE_DIR, ".env"))
+env = environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
